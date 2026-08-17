f35d08ad5b3a10249b654d472ee9b450a7672d45
Sapon <sapon@MacBookAirSapon.local>
2026-08-17 12:41:10 +0700

    5.2 complete

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,21 @@
-import Link from "src/app"
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-
-<nav className="navbar" aria-label="เมนูหลัก"> 
+    <nav className="navbar" aria-label="เมนูหลัก"> 
       <ul className="navList"> 
         <li> 
-          <Link className="navLink" href="/page.tsx"> 
+          <Link className="navLink" href="/"> 
             หน้าแรก 
           </Link> 
         </li> 
         <li> 
-          <Link className="navLink" href="courses/page.tsx"> 
+          <Link className="navLink" href="/courses"> 
             รายวิชา 
           </Link> 
         </li> 
         <li> 
-          <Link className="navLink" href="about/page.tsx"> 
+          <Link className="navLink" href="/about"> 
             เกี่ยวกับ 
           </Link> 
         </li> 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navbar from "./components/Navbar";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -20,10 +21,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="th"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      >
+      <body >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
